3f16c2b53bbf3ba40fe720ed9b1b213703a07ae0
Loyio Hex <i@loyio.me>
2022-09-11 20:53:02 +0800

    update shader, add transform, coordinate system (#6)

---
+++ LearnOpenGL/TransformationsBasic/LearnTransformations.cpp
@@ -0,0 +1,210 @@
+//
+//  LearnTransformations.cpp
+//  LearnOpenGL
+//
+//  Created by Loyio Hex on 9/10/22.
+//
+
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+#include "Common.h"
+#include "Shader.hpp"
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void processInput(GLFWwindow* window);
+
+int main()
+{
+    // Transformation Test Start
+    glm::vec4 vec(1.0f, 0.0f, 0.0f, 1.0f);
+    
+    glm::mat4 trans = glm::mat4(1.0f);
+    trans = glm::translate(trans, glm::vec3(1.0f, 1.0f, 0.0f));
+    vec = trans * vec;
+    std::cout << vec.x << " " << vec.y << " " << vec.z << std::endl;
+    
+    // Transfromation Test End
+    
+    
+    Initialize();
+    
+    GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL - Loyio", NULL, NULL);
+    
+#ifdef __APPLE__
+    glfwMakeContextCurrent(window);
+#endif
+    
+    if(window == NULL)
+    {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    
+    if(!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
+    
+    // 导入编译Shader
+    Shader ourShader("TransformationsBasic/transform.vs", "TransformationsBasic/transform.fs");
+    
+    // 坐标
+    float vertices[] =
+    {
+        // 位置               纹理坐标
+         0.5f,  0.5f, 0.0f,  1.0f, 1.0f,
+         0.5f, -0.5f, 0.0f,  1.0f, 0.0f,
+        -0.5f, -0.5f, 0.0f,  0.0f, 0.0f,
+        -0.5f,  0.5f, 0.0f,  0.0f, 1.0f
+    };
+    unsigned int indices[] =
+    {
+        0, 1, 3, // 三角形1
+        1, 2, 3  // 三角形2
+    };
+    unsigned int VBO, VAO, EBO;
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+    glGenBuffers(1, &EBO);
+    
+    glBindVertexArray(VAO);
+    
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+    
+    // 位置属性
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+    
+    // 纹理
+    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
+    glEnableVertexAttribArray(1);
+    
+    // 创建纹理
+    unsigned int texture1, texture2;
+    
+    // texture1
+    glGenTextures(1, &texture1);
+    glBindTexture(GL_TEXTURE_2D, texture1);
+    
+    // 纹理环绕
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    
+    // 纹理过滤
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    
+    // 导入图像-箱子
+    int width, height, nrChannels;
+    unsigned char *data = stbi_load("LearnTextures/container.jpg", &width, &height, &nrChannels, 0);
+    if(data)
+    {
+        // 生成纹理
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+        glGenerateMipmap(GL_TEXTURE_2D);
+    }else
+    {
+        std::cout << "Failed to load texture" << std::endl;
+    }
+    // 释放图像内存
+    stbi_image_free(data);
+    
+    
+    // texture2
+    glGenTextures(1, &texture2);
+    glBindTexture(GL_TEXTURE_2D, texture2);
+    
+    // 纹理环绕
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    
+    // 纹理过滤
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    
+    // 导入图像
+    
+    // 翻转Y轴
+    stbi_set_flip_vertically_on_load(true);
+    
+    data = stbi_load("LearnTextures/awesomeface.png", &width, &height, &nrChannels, 0);
+    if(data)
+    {
+        // 生成纹理
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
+        glGenerateMipmap(GL_TEXTURE_2D);
+    }else
+    {
+        std::cout << "Failed to load texture" << std::endl;
+    }
+    // 释放图像内存
+    stbi_image_free(data);
+    
+    ourShader.use();
+    // 直接通过纹理类设置
+    ourShader.setInt("texture1", 0);
+    ourShader.setInt("texture2", 1);
+    
+    
+    while(!glfwWindowShouldClose(window))
+    {
+        // 处理输入
+        processInput(window);
+        
+        // 渲染
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+        
+        // 绑定纹理
+        glActiveTexture(GL_TEXTURE0);
+        glBindTexture(GL_TEXTURE_2D, texture1);
+        glActiveTexture(GL_TEXTURE1);
+        glBindTexture(GL_TEXTURE_2D, texture2);
+        
+        // 创建变换
+        glm::mat4 trans = glm::mat4(1.0f);
+//        trans = glm::rotate(trans, glm::radians(90.0f), glm::vec3(0.0, 0.0, 1.0));
+        trans = glm::translate(trans, glm::vec3(0.5f, -0.5f, 0.0f));
+        trans = glm::rotate(trans, (float)glfwGetTime(),  glm::vec3(0.0, 0.0, 1.0));
+        
+        // Shader
+        ourShader.use();
+        unsigned int transformLoc = glGetUniformLocation(ourShader.ID, "transform");
+        glUniformMatrix4fv(transformLoc, 1, GL_FALSE, glm::value_ptr(trans));
+        
+        // 渲染木箱
+        glBindVertexArray(VAO);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+    
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
+    glDeleteBuffers(1, &EBO);
+    
+    glfwTerminate();
+    return 0;
+}
+
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+    glViewport(0, 0, width, height);
+}
+
+void processInput(GLFWwindow* window)
+{
+    if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+}
